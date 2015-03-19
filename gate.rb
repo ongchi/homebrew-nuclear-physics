6a40335a0ae2fec833c96fb121aa6725fd6fdbf7
ongchi <ihcgno@icloud.com>
2015-03-19 10:58:26 +0800

    Revert "cmake testing"
    This reverts commit 029b14b1e7a005f80018993464864bf88e514e02.

--- gate.rb
+++ gate.rb
@@ -6,9 +6,6 @@ class Gate < Formula
   version "7.0"
 
   depends_on 'cmake' => :build
-  depends_on 'geant4' => ["with-gdml", "with-qt"]
-  depends_on 'root' => "with-gdml"
-  depends_on 'qt'
 
   needs :cxx11
 
--- geant4.rb
+++ geant4.rb
@@ -2,8 +2,15 @@ require 'formula'
 
 class Geant4 < Formula
   homepage 'http://geant4.cern.ch'
-  url 'http://geant4.web.cern.ch/geant4/support/source/geant4.9.6.p04.tar.gz'
-  sha1 'f1cdab8cbbdf2397fbe4f12260055cf6c2f202d6'
+  url 'http://geant4.cern.ch/support/source/geant4.10.01.tar.gz'
+  sha1 'd888b992b789a0d0e0a1a13debc9a51dae5e3743'
+
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "4cbef3a10978ba3cbbd869415359291369cb4219" => :yosemite
+    sha1 "0024b1c66a2a725f051d040212549ab040535fae" => :mavericks
+    sha1 "b3f7a2bff728e0f54b6186ef92ce6837253c3d45" => :mountain_lion
+  end
 
   option 'with-g3tog4', 'Use G3toG4 Library'
   option 'with-gdml', 'Use GDML'
@@ -11,30 +18,32 @@ class Geant4 < Formula
 
   depends_on 'cmake' => :build
   depends_on :x11
-  depends_on 'clhep' => :optional
+  depends_on 'clhep'
   depends_on 'qt' => :optional
   depends_on 'xerces-c' if build.with? 'gdml'
+
+  def cmake_opt(opt, pkg = opt)
+    "-D#{opt}=#{(build.with? pkg) ? "ON" : "OFF"}"
+  end
 
   def install
     mkdir 'geant-build' do
       args = %W[
                ../
-               -Dgnuinstall=ON
-               -Dbuiltin_freetype=ON
                -DGEANT4_INSTALL_DATA=ON
                -DGEANT4_USE_OPENGL_X11=ON
                -DGEANT4_USE_RAYTRACER_X11=ON
                -DGEANT4_BUILD_EXAMPLE=ON
+               -DGEANT4_USE_SYSTEM_CLHEP=ON
                ]
 
       args << '-DGEANT4_INSTALL_DATA_TIMEOUT=86400' if build.with? 'notimeout'
       args << '-DGEANT4_USE_QT=ON' if build.with? 'qt'
       args << '-DGEANT4_USE_G3TOG4=ON' if build.with? 'g3tog4'
       args << '-DGEANT4_USE_GDML=ON' if build.with? 'gdml'
-      args << '-DGEANT4_USE_SYSTEM_CLHEP=ON' if build.without? 'clhep'
       args.concat(std_cmake_args)
-      system "cmake", *args
-      system "make", "install"
+      system "cmake", "..", "-Dgnuinstall=ON", "-Dbuiltin_freetype=ON",
+      system "make install"
     end
   end
 end
